88c3e29dff96f0e10ed222f946b1a5440a7d4cf2
Obony <faithnyabz69@gmail.com>
2023-05-03 12:38:51 -0700

    Textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,5 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
+a = ARGV[0].scan(/(?<=from:)\+?\w+/).join
+b = ARGV[0].scan(/(?<=to:)\+?\w+/).join
+c = ARGV[0].scan(/(?<=flags:)[-:0-9]+/).join
+printf("%s,%s,%s\n", a, b, c)
