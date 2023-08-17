742995ea0b4299208b67530a9f7b86e89fc40371
Obony <faithnyabz69@gmail.com>
2023-08-17 19:27:20 +0300

    webstack tasks

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
-exec { 'increase_workers':
-  command => '/bin/sed -i "s/worker_processes 4/worker_processes 8/g" /etc/nginx/nginx.conf',
-  notify  => Service['nginx'],
+# Fix nginx to many failed requests
+exec {'increase_workers':
+command => '/bin/sed -i "s/worker_processes 4/worker_processes 8/g" /etc/nginx/nginx.conf',
+notify  => Service['nginx'],
 }
 
-service { 'nginx':
-  ensure => 'running',
+service {'nginx':
+ensure => 'running',
 }
-
