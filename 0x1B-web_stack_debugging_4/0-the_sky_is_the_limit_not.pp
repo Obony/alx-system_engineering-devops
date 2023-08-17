ac23096369442eb4a65dfcfb286e3b7ba381418e
Obony <faithnyabz69@gmail.com>
2023-08-17 19:24:23 +0300

    webstack tasks

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,9 @@
-# Increases the amount of traffic an Nginx server can handle.
+# Fix nginx to many failed requests
+exec {'increase_workers':
+command => '/bin/sed -i "s/worker_processes 4/worker_processes 8/g" /etc/nginx/nginx.conf',
+notify  => Service['nginx'],
+}
 
-# Increase the ULIMIT of the default file
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-} ->
-
-# Restart Nginx
-exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+service {'nginx':
+ensure => 'running',
 }
